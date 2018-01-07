e32698d82c7c4580314ddd5295e20773f9b6aded
Prem Udeshi <premudeshi99@gmail.com>
2018-01-07 18:15:09 +0300

    Refrences
    Updated Refrences

---
+++ en/500.py
@@ -0,0 +1,21 @@
+#use usb rules,
+#make sure you give permission 
+
+#!/usr/bin/python
+import serial
+import time
+ser = serial.Serial('/dev/Vital-Water-Master', 9600)
+time.sleep(2)
+ser.write('b')
+count = 0
+time.sleep(1)
+readcomplete = ser.read(3)
+
+while True:
+	if readcomplete == '255':
+		break
+	else:
+		print "No"
+
+print "Finished Code"
+
